b5c32f1c78a180522527d8cf192922d551d4095c
alexandre <alexandre@laptop-alex.focus>
2019-07-29 18:11:58 +0200

    fix(env): rename MAUTIC_TABLE_PREFIX

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,7 +2,7 @@
 set -e
 
 if [[ -n "${MAUTIC_DB_HOST}" ]]; then
-    exec "$@" -host="${MAUTIC_DB_HOST}" -port="${MAUTIC_DB_PORT}" -user="${MAUTIC_DB_USER}" -db="${MAUTIC_DB_NAME}" -tableprefix="${MAUTIC_DB_TABLE_PREFIX}" -pass="${MAUTIC_DB_PASSWORD}"
+    exec "$@" -host="${MAUTIC_DB_HOST}" -port="${MAUTIC_DB_PORT}" -user="${MAUTIC_DB_USER}" -db="${MAUTIC_DB_NAME}" -tableprefix="${MAUTIC_TABLE_PREFIX}" -pass="${MAUTIC_DB_PASSWORD}"
 else
     exec "$@"
 fi
