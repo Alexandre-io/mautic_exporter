175a965a65d5751464d7660566fe52d4f4f006b6
Alexandre <Alexandre-io@users.noreply.github.com>
2021-03-10 15:04:52 +0100

    feat(exporter): add segments & campaigns metrics

--- mautic_exporter.go
+++ mautic_exporter.go
@@ -26,6 +26,7 @@ type mauticCollector struct {
 	numAnonymousMetric       *prometheus.Desc
 	numEmailsMetric          *prometheus.Desc
 	numCampaignsMetric       *prometheus.Desc
+	numSegmentsMetric        *prometheus.Desc
 	numPageHitsMetric        *prometheus.Desc
 	numWebhookQueueMetric    *prometheus.Desc
 	numMessageQueueMetric    *prometheus.GaugeVec
@@ -33,6 +34,8 @@ type mauticCollector struct {
 	numCampaignEventsMetric  *prometheus.GaugeVec
 	numNotificationsMetric   *prometheus.Desc
 	numPageRedirectMetric    *prometheus.Desc
+	numLeadsinCampaignMetric *prometheus.GaugeVec
+	numLeadsinSegmentMetric  *prometheus.GaugeVec
 
 	dbHost        string
 	dbName        string
@@ -58,6 +61,10 @@ func newMauticCollector(host string, dbname string, username string, pass string
 		),
 		numCampaignsMetric: prometheus.NewDesc(prometheus.BuildFQName(namespace, "", "campaigns_total"),
 			"Shows the number of campaigns in Mautic",
+			nil, nil,
+		),
+		numSegmentsMetric: prometheus.NewDesc(prometheus.BuildFQName(namespace, "", "segments_total"),
+			"Shows the number of segments in Mautic",
 			nil, nil,
 		),
 		numPageHitsMetric: prometheus.NewDesc(prometheus.BuildFQName(namespace, "", "page_hits_total"),
@@ -94,6 +101,20 @@ func newMauticCollector(host string, dbname string, username string, pass string
 			"Shows the number of page redirects in Mautic",
 			nil, nil,
 		),
+		numLeadsinCampaignMetric: prometheus.NewGaugeVec(prometheus.GaugeOpts{
+			Namespace: namespace,
+			Name:      "leads_in_campaign_total",
+			Help:      "Shows the number of leads in a campaign",
+		},
+			[]string{"type"},
+		),
+		numLeadsinSegmentMetric: prometheus.NewGaugeVec(prometheus.GaugeOpts{
+			Namespace: namespace,
+			Name:      "leads_in_segment_total",
+			Help:      "Shows the number of leads in a segment",
+		},
+			[]string{"type"},
+		),
 
 		dbHost:        host,
 		dbName:        dbname,
@@ -111,6 +132,7 @@ func (collector *mauticCollector) Describe(ch chan<- *prometheus.Desc) {
 	ch <- collector.numAnonymousMetric
 	ch <- collector.numEmailsMetric
 	ch <- collector.numCampaignsMetric
+	ch <- collector.numSegmentsMetric
 	ch <- collector.numPageHitsMetric
 	ch <- collector.numWebhookQueueMetric
 	ch <- collector.numLeadEventFailedMetric
@@ -118,6 +140,8 @@ func (collector *mauticCollector) Describe(ch chan<- *prometheus.Desc) {
 	ch <- collector.numPageRedirectMetric
 	collector.numMessageQueueMetric.Describe(ch)
 	collector.numCampaignEventsMetric.Describe(ch)
+	collector.numLeadsinCampaignMetric.Describe(ch)
+	collector.numLeadsinSegmentMetric.Describe(ch)
 }
 
 //Collect method is required for a prometheus.Collector type
@@ -135,57 +159,78 @@ func (collector *mauticCollector) Collect(ch chan<- prometheus.Metric) {
 
 	//select count(id) as numLeads from leads where date_identified is not null
 	queryNumLeadsMetric := fmt.Sprintf("select count(id) as numLeads from %sleads where date_identified is not null;", collector.dbTablePrefix)
-	mtQueryDesc(db, ch, collector.numLeadsMetric, queryNumLeadsMetric)
+	mtQueryGauge(db, ch, collector.numLeadsMetric, queryNumLeadsMetric)
 
 	//select count(id) as numAnonymous from leads where date_identified is null
 	queryNumAnonymousMetric := fmt.Sprintf("select count(id) as numAnonymous from %sleads where date_identified is null;", collector.dbTablePrefix)
-	mtQueryDesc(db, ch, collector.numAnonymousMetric, queryNumAnonymousMetric)
+	mtQueryGauge(db, ch, collector.numAnonymousMetric, queryNumAnonymousMetric)
 
 	//select count(*) as numEmails from emails;
 	queryNumEmailsMetric := fmt.Sprintf("select count(*) as numEmails from %semails;", collector.dbTablePrefix)
-	mtQueryDesc(db, ch, collector.numEmailsMetric, queryNumEmailsMetric)
+	mtQueryGauge(db, ch, collector.numEmailsMetric, queryNumEmailsMetric)
 
 	//select count(*) as numCampaigns from campaigns;
 	queryNumCampaignsMetric := fmt.Sprintf("select count(*) as numCampaigns from %scampaigns;", collector.dbTablePrefix)
-	mtQueryDesc(db, ch, collector.numCampaignsMetric, queryNumCampaignsMetric)
+	mtQueryGauge(db, ch, collector.numCampaignsMetric, queryNumCampaignsMetric)
+
+	//select count(*) as numCampaigns from campaigns;
+	queryNumSegmentsMetric := fmt.Sprintf("select count(*) as numSegments from %slead_lists;", collector.dbTablePrefix)
+	mtQueryGauge(db, ch, collector.numSegmentsMetric, queryNumSegmentsMetric)
 
 	//select count(*) as numPageHits from page_hits;
 	queryNumPageHitsMetric := fmt.Sprintf("select count(*) as numPageHits from %spage_hits;", collector.dbTablePrefix)
-	mtQueryDesc(db, ch, collector.numPageHitsMetric, queryNumPageHitsMetric)
+	mtQueryCounter(db, ch, collector.numPageHitsMetric, queryNumPageHitsMetric)
 
 	//select count(*) as numWebhookQueue from webhook_queue;
 	queryNumWebhookQueueMetric := fmt.Sprintf("select count(*) as numWebhookQueue from %swebhook_queue;", collector.dbTablePrefix)
-	mtQueryDesc(db, ch, collector.numWebhookQueueMetric, queryNumWebhookQueueMetric)
+	mtQueryGauge(db, ch, collector.numWebhookQueueMetric, queryNumWebhookQueueMetric)
 
 	//select status as label, count(*) as value from message_queue group by status;
 	queryNumMessageQueueMetric := fmt.Sprintf("select status as label, count(*) as value from %smessage_queue group by status;", collector.dbTablePrefix)
 	mtQueryGaugeVec(db, ch, collector.numMessageQueueMetric, queryNumMessageQueueMetric)
 
 	//select type as label, count(*) as value from campaign_events GROUP BY type;
-	queryNumCampaignEventsMetric := fmt.Sprintf("select type as label, count(*) as value from %scampaign_events GROUP BY type;", collector.dbTablePrefix)
+	queryNumCampaignEventsMetric := fmt.Sprintf("select type as label, count(*) as value from %scampaign_events group by type;", collector.dbTablePrefix)
 	mtQueryGaugeVec(db, ch, collector.numCampaignEventsMetric, queryNumCampaignEventsMetric)
 
 	//select count(*) from campaign_lead_event_failed_log;
 	queryNumLeadEventFailedMetric := fmt.Sprintf("select count(*) from %scampaign_lead_event_failed_log;", collector.dbTablePrefix)
-	mtQueryDesc(db, ch, collector.numLeadEventFailedMetric, queryNumLeadEventFailedMetric)
+	mtQueryCounter(db, ch, collector.numLeadEventFailedMetric, queryNumLeadEventFailedMetric)
 
-	//select count(*) from campaign_lead_event_failed_log;
+	//select count(*) from notifications;
 	queryNumNotificationsMetric := fmt.Sprintf("select count(*) from %snotifications;", collector.dbTablePrefix)
-	mtQueryDesc(db, ch, collector.numNotificationsMetric, queryNumNotificationsMetric)
+	mtQueryGauge(db, ch, collector.numNotificationsMetric, queryNumNotificationsMetric)
 
-	//select count(*) from campaign_lead_event_failed_log;
+	//select count(*) from page_redirects;
 	queryNumPageRedirectMetric := fmt.Sprintf("select count(*) from %spage_redirects;", collector.dbTablePrefix)
-	mtQueryDesc(db, ch, collector.numPageRedirectMetric, queryNumPageRedirectMetric)
+	mtQueryCounter(db, ch, collector.numPageRedirectMetric, queryNumPageRedirectMetric)
+
+	//select campaign_id as label, count(*) as value from campaign_leads where manually_removed = 0  group by campaign_id;
+	queryNumLeadsinCampaignMetric := fmt.Sprintf("select campaign_id as label, count(*) as value from %scampaign_leads where manually_removed = 0 group by campaign_id;", collector.dbTablePrefix)
+	mtQueryGaugeVec(db, ch, collector.numLeadsinCampaignMetric, queryNumLeadsinCampaignMetric)
+
+	//select leadlist_id as label, count(*) as value from lead_lists_leads group by leadlist_id ;
+	queryNumLeadsinSegmentMetric := fmt.Sprintf("select leadlist_id as label, count(*) as value from %slead_lists_leads group by leadlist_id;", collector.dbTablePrefix)
+	mtQueryGaugeVec(db, ch, collector.numLeadsinSegmentMetric, queryNumLeadsinSegmentMetric)
 
 }
 
-func mtQueryDesc(db *sql.DB, ch chan<- prometheus.Metric, desc *prometheus.Desc, mysqlRequest string) {
+func mtQueryCounter(db *sql.DB, ch chan<- prometheus.Metric, desc *prometheus.Desc, mysqlRequest string) {
 	var value float64
 	var err = db.QueryRow(mysqlRequest).Scan(&value)
 	if err != nil {
 		log.Fatal(err)
 	}
 	ch <- prometheus.MustNewConstMetric(desc, prometheus.CounterValue, value)
+}
+
+func mtQueryGauge(db *sql.DB, ch chan<- prometheus.Metric, desc *prometheus.Desc, mysqlRequest string) {
+	var value float64
+	var err = db.QueryRow(mysqlRequest).Scan(&value)
+	if err != nil {
+		log.Fatal(err)
+	}
+	ch <- prometheus.MustNewConstMetric(desc, prometheus.GaugeValue, value)
 }
 
 func mtQueryGaugeVec(db *sql.DB, ch chan<- prometheus.Metric, desc *prometheus.GaugeVec, mysqlRequest string) {
